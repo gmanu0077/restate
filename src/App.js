7f48781e507fe50655eb169b9a0dd5ebe6fa542e
gmanu0077 <66879853+gmanu0077@users.noreply.github.com>
2022-11-17 10:19:34 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import Home from './pages/home';
 import Book from './pages/bookmark';
 import {BrowserRouter,  Route,Routes } from 'react-router-dom'
 import './app.css'
-import { useState } from 'react';
 function App() {
-  const[bookmark,setbookmark]=useState([])
   return (
     <div className="App">
     <BrowserRouter>
     <Routes>
-    <Route exact path="/" element={<Home setbookmark={setbookmark} />}/>
-    <Route exact path="/bookmark" element={<Book hotels={bookmark} />}/>
+    <Route exact path="/" element={<Home  />}/>
+    <Route  path="/bookmark" element={<Book />}/>
     </Routes>
     </BrowserRouter>
     </div>
