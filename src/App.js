0c8f6b45d8ef7313efab606ca3bebc1b120f959b
gmanu0077 <66879853+gmanu0077@users.noreply.github.com>
2022-12-14 00:27:08 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Home from './pages/home';
 import Book from './pages/bookmark';
-import {BrowserRouter,  Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './app.css'
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-    <Route exact path="/" element={<Home  />}/>
-    <Route  path="/bookmark" element={<Book />}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="api/bookmark" element={<Book />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,20 +2,20 @@ import "./nav.css"
 function Nav() {
   return (
     <div>
-    <nav class="navbar">
-    <div class="navbar-container ">
-        
-        <ul class="menu-items">
-            <li><a href="/bookmark">Bookmark</a></li>
+      <nav class="navbar">
+        <div class="navbar-container ">
+
+          <ul class="menu-items">
+            <li><a href="api/bookmark">Bookmark</a></li>
             <li><a href="/">Home</a></li>
             <li><a href="#">Sell</a></li>
             <li><a href="#">Manage property</a></li>
-            
+
             <li><a href="#">Resources</a></li>
-        </ul>
-        <h1 class="brand-logo right pink-text logo">Estatery</h1>
-    </div>
-</nav></div>
+          </ul>
+          <h1 class="brand-logo right pink-text logo">Estatery</h1>
+        </div>
+      </nav></div>
   );
 }
 
